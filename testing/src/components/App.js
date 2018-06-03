d62666f8b57c49ad40c63787278132a3914a981f
doka <kaegidominik@gmail.com>
2018-06-03 09:32:23 +0200

    HOC Code

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+import * as actions from 'actions';
 
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  )
+class App extends Component {
+  renderButton() {
+    return (
+      this.props.auth
+        ? (
+         <button
+            onClick={() => this.props.changeAuth(false)}>
+            Sign Out
+          </button>
+        )
+        : <button
+            onClick={() => this.props.changeAuth(true)}>
+            Sign In
+          </button>
+    );
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Add Comment</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route exact path="/posts" component={CommentBox} />
+        <Route exact path="/" component={CommentList} />
+      </div>
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
+}
+
+export default connect(mapStateToProps, actions)(App);
